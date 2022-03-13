dfbd405dd49ed2b9211811ca66a32f24ba8f5170
clout <59515681+cloutjs@users.noreply.github.com>
2022-03-13 23:13:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 while True:
     n = random.randint(10000000, 99999999)
     r = requests.get(f"https://www.stundenplan24.de/{str(n)}/mobil")
-    if r.status_code == 200:
+    if r.status_code == 401:
         print("[+] " + str(n))
         open('hits.txt','a+').write("{}\n".format(str(n)))
     elif r.status_code == 404:
